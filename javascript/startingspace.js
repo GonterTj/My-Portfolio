f45f6482a4c24eab5f2b237641b38bc266146440
GonterTj <122777514+GonterTj@users.noreply.github.com>
2023-04-13 12:13:37 +0200

    a

---
+++ javascript/startingspace.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+class startingspace extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        
+
+        
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
